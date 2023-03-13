e123b4df11ee59acb1af076cdbdef29e71623d7a
Eric Villasenor <eric.villasenor@intel.com>
2023-03-14 00:15:32 +0100

    adding default vim settings

---
+++ after/plugin/defaults.lua
@@ -0,0 +1,40 @@
+-- set whitespace symbols
+vim.opt.listchars = {
+  tab = '→ ',
+  trail = '•',
+  extends = '»',
+  precedes = '«',
+  nbsp = '␣'
+}
+vim.opt.list = true
+
+-- remove command line, unless in use
+vim.opt.cmdheight = 0
+
+-- relative line numbering
+vim.opt.relativenumber = false
+
+-- highlight line number
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = "number"
+
+-- file space settings
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+-- un-highlight search
+vim.keymap.set({ 'n', 'v' }, '<leader>,', ':nohlsearch<CR>', { silent = true })
+
+-- remove trailing whitespace on save
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = {"*"},
+  callback = function()
+    local save_cursor = vim.fn.getpos(".")
+    vim.cmd([[%s/\s\+$//e]])
+    vim.fn.setpos(".", save_cursor)
+  end,
+})
+
+-- vim: ts=2 sts=2 sw=2 et
