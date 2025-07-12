b0ff63c4a7dc86d5fb4f85664b249b1e029e740a
Eric Villasenor <eric.villasenor@intel.com>
2025-07-11 23:59:56 -0700

    onedark backup theme added

---
+++ lua/custom/plugins/onedark.lua
@@ -0,0 +1,16 @@
+-- Onedark theme https://github.com/navarasu/onedark.nvim
+
+return {
+  {
+    --'navarasu/onedark.nvim',
+    'olimorris/onedarkpro.nvim',
+    priority = 1000,
+    -- config = function()
+    --   require('onedark').setup({
+    --     style = 'cool',
+    --     transparent = false,
+    --     term_colors = true,
+    --   })
+    -- end
+  },
+}
