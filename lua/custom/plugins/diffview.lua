4d939fdabefc2ab193c1a9f1039c2099a4739dc4
Eric Villasenor <eric.villasenor@intel.com>
2025-07-11 23:59:40 -0700

    diffview added

---
+++ lua/custom/plugins/diffview.lua
@@ -0,0 +1,12 @@
+-- Diffview plugin configuration
+
+return {
+  "sindrets/diffview.nvim",
+  version = "*",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not required, but recommended
+  },
+}
+
+-- vim: ts=2 sts=2 sw=2 et
