2c98785f7de493f74811b9935a4f6f19388bc652
Eric Villasenor <eric.villasenor@intel.com>
2025-08-01 19:31:17 -0700

    comments to codecompanion

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -1,13 +1,16 @@
 
+-- Plugin specification for olimorris/codecompanion.nvim
+-- This table is returned to configure the plugin and its dependencies
 return {
   {
-    "olimorris/codecompanion.nvim",
-    opts = {},
+    "olimorris/codecompanion.nvim", -- Main plugin
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      "zbirenbaum/copilot.lua",
+      "nvim-lua/plenary.nvim", -- Utility functions
+      "nvim-treesitter/nvim-treesitter", -- Syntax parsing
+      "zbirenbaum/copilot.lua", -- GitHub Copilot integration
     },
+    opts = {}, -- Plugin options (empty for now)
   },
 }
+
 -- vim: ts=2 sts=2 sw=2 et
